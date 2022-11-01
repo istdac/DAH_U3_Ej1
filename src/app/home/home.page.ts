0fce29bb12741d6ef5b7b3670e8080278f1cc395
DiegoCadena <diego.cadena@borngroup.com>
2022-11-01 08:31:21 -0700

    manejar consulta general en vista

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
+import { StudentService } from './../services/student.service';
 import { Component } from '@angular/core';
-
+import { Student } from '../models/student';
+import { StudentService } from '../services/student.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  //Manejar vista
+  /*Import student y servicio 
+    Como servicio inyectable, se inserta en constructor
+  */
+ //Atributo espejo del arreglo
+  public students: Student[]
+  constructor(private studentService: StudentService) {
+    //Hacer consulta general
+    this.students = this.studentService.getStudents();
 
-  constructor() {}
+  }//constructor
 
-}
+}//homepage
