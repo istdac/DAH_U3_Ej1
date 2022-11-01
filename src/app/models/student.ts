e4d8ee23a3b4caf7064c449f2dab2581df06fc37
DiegoCadena <diego.cadena@borngroup.com>
2022-11-01 08:23:27 -0700

    popular datos defecto

--- src/app/models/student.ts
+++ src/app/models/student.ts
@@ -1,5 +1,5 @@
 export class Student {
-    controlNumber: string; //Llave primaria
+    controlNumber: string; //Llave primaria // Si se usa un ? al final del nombre, significa que es opcional
     name: string;
     curp: string;
     age: number;
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
-
+import { Student } from '../models/student';
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
+  //Construir lo de la consulta general
+  /*Hacer import de student */
+  private students: Student[];
 
-  constructor() { }
+  constructor() {
+    //Llenar algunos datos por defecto
+    this.students = [
+      {
+        controlNumber: '02400391',
+        age:38,
+        career: 'ISC',
+        curp: 'AOVI840917HNTRZS09',
+        email: 'iarjona@ittepic.edu.mx',
+        name: 'Israel Arjona Vizcaíno',
+        nip: 717
+      },
+      {
+        controlNumber: '12404391',
+        age:25,
+        career: 'IGE',
+        curp: 'AVTIQ40917HNTRZS09',
+        email: 'iarjona2@ittepic.edu.mx',
+        name: 'Israel Arjona Vizcaídos',
+        nip: 616
+      },
+      {
+        controlNumber: '32405891',
+        age:18,
+        career: 'IEL',
+        curp: 'AOVIT40917HNTRZS09',
+        email: 'iarjona3@ittepic.edu.mx',
+        name: 'Israel Arjona Vizcaítres',
+        nip: 327
+      }
+    ];
+   }
 }
