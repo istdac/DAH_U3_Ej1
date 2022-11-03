fbecdf00cbc9f771faf4c5cb6e86d942e3e558c0
DiegoCadena <diego.cadena@borngroup.com>
2022-11-03 08:34:34 -0700

    obtener alumno en pagina nueva

--- src/app/view-student/view-student.page.ts
+++ src/app/view-student/view-student.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../models/student';
+import { StudentService } from '../services/student.service';
 
 @Component({
   selector: 'app-view-student',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewStudentPage implements OnInit {
 
-  constructor() { }
+  public student: Student;
+  //Preparar servicio para obtener datos
+  constructor(private studentService: StudentService) { }
 
+  //Qué hacer cuando se abra la ventana
+  /*
+    Primero se ejecuta el constructor, ideal para consultas 
+    ngOnInit se ejecuta después del constructor, el primer método que corre en el ciclo de vida, cuando se lanza la pagina
+
+  */
   ngOnInit() {
+    /*Hacer las consultas aqui para ver cómo se va cargando*/
+    this.student = this.studentService.getStudentByControlNumber('32405891');
+    
   }
 
 }
