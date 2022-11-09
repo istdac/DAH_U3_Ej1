b80b49d60ed17b9fbc5bdcec52f807943b4f8018
DiegoCadena <diego.cadena@borngroup.com>
2022-11-09 08:28:13 -0700

    create form and form service

--- src/app/new-student/new-student.page.ts
+++ src/app/new-student/new-student.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from '../models/student';
 import { StudentService } from '../services/student.service';
-
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 @Component({
   selector: 'app-new-student',
   templateUrl: './new-student.page.html',
@@ -11,8 +11,9 @@ export class NewStudentPage implements OnInit {
 
   //Objeto estudiante para mandar datos
   public stu: Student;
-
-  constructor(private stuServ: StudentService) {
+  public myForm: FormGroup;
+  /*Form builder es un servicio */
+  constructor(private stuServ: StudentService, private fb: FormBuilder) {
 
   }//constructor
 
