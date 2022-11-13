6945c11fdca8c8df69e2375880bdabe167eb08a3
DiegoCadena <diego.cadena@borngroup.com>
2022-11-13 12:58:58 -0700

    form validation & styling

--- src/app/new-student/new-student.page.ts
+++ src/app/new-student/new-student.page.ts
@@ -28,8 +28,36 @@ export class NewStudentPage implements OnInit {
           Validators.minLength(7),
           Validators.maxLength(8),
           Validators.pattern('^[0-9]+$')
+        ])],
+        name:['',Validators.compose([
+          Validators.required,
+          Validators.minLength(1)
+        ])],
+        curp:['',Validators.compose([
+          Validators.pattern('/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$'),
+          Validators.required,
+          Validators.minLength(2)
+        ])],
+        age:[17,Validators.compose([
+          Validators.min(17),
+          Validators.required
+        ])],
+        nip:['',Validators.compose([
+          Validators.required,
+          Validators.min(9),
+          Validators.max(9999),
+        ])],
+        email:['',Validators.compose([
+          Validators.required,
+          Validators.pattern('[a-zA-Z]+@ittepic.edu.mx'),
+        ])],
+        career:['ISC',Validators.compose([
+          Validators.required,
+        ])],
+        photo:['',Validators.compose([
+          Validators.required,
+          Validators.pattern('/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/')
         ])]
-
       }
     ); //Group recibe un objeto
 
@@ -39,8 +67,38 @@ export class NewStudentPage implements OnInit {
         {type:'required',message:'Número de control obligatorio'},
         {type:'minlength',message:'El número de control debe ser de 8 dígitos'},
         {type:'maxlength',message:'El número de control debe ser de 8 dígitos'},
-        {type:'pattern',message:'El número de control está mal formado'},
-      ]
+        {type:'pattern',message:'El número de control está mal formado'}
+      ],
+      name:[
+        {type:'required',message:'Nombre obligatorio'},
+        {type:'minlength',message:'El nombre debe ser de al menos 1 dígito'},
+      ],
+      curp:[
+        {type:'required',message:'CURP obligatorio'},
+        {type:'minlength',message:'El número de control debe ser de al menos 2 dígitos'},
+        {type:'pattern',message:'El CURP está mal formado'},
+      ],
+      age:[
+        {type:'required',message:'Edad obligatoria'},
+        {type:'min',message:'La edad debe ser de al menos 17 años'},
+      ],
+      nip:[
+        {type:'required',message:'NIP obligatorio'},
+        {type:'min',message:'El nip debe ser un valor mayor que 9'},
+        {type:'max',message:'El nip debe ser un valor menor que 9999'},
+      ],
+      email:[
+        {type:'required',message:'Correo obligatorio'},
+        {type:'pattern',message:'El correo está mal formado'},
+      ],
+      career:[
+        {type:'required',message:'Carrera obligatorio'},
+      ],
+      photo:[
+        {type:'required',message:'Foto obligatoria'},
+        {type:'pattern',message:'El url de la foto está mal formado'},
+      ],
+
     };
   }//ngoninit
 
